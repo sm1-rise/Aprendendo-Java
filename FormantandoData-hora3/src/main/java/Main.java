5965ef549972db6be9256c589a9a65b2d5b3c68d
Samuel Martins <samuel.mrtins2016@gmail.com>
2023-05-05 13:22:23 -0300

    teste

---
+++ FormantandoData-hora3/src/main/java/Main.java
@@ -0,0 +1,25 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+
+        LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+        LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+        LocalDateTime r3 = LocalDateTime.ofInstant(d06,ZoneId.systemDefault());
+        LocalDateTime r4 = LocalDateTime.ofInstant(d06,ZoneId.of("Portugal"));
+
+        System.out.println(r1);
+        System.out.println(r2);
+        System.out.println(r3);
+        System.out.println(r4 );
+    }
+    
+}
