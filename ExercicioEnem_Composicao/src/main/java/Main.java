1199d51133a140b71fc78c21c1089c731049216b
Samuel Martins <samuel.mrtins2016@gmail.com>
2023-05-12 22:13:39 -0300

    Classe HourContract finalizada

--- ExercicioEnem_Composicao/src/main/java/Main.java
+++ ExercicioEnem_Composicao/src/main/java/Main.java
@@ -4,15 +4,36 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+
         ArrayList<WorkerLevel> hourContract = new ArrayList<>();
-        Scanner sdtin = new Scanner(System.in);
+        Scanner stdin = new Scanner(System.in);
 
         System.out.println("Enter departament's name: ");
-        String departament = sdtin.nextLine();
+        int code = stdin.nextInt();
         System.out.println("Enter worker data: ");
         System.out.println("Name: ");
-        String nameWorker = sdtin.nextLine();
+        String nameWorker = stdin.nextLine();
         System.out.println("Level: ");
+        int level = stdin.nextInt();
+        System.out.println("Base Salary: ");
+        double baseSalary = stdin.nextDouble();
+        System.out.println("How many contract to this worker ?");
+        int qtContract = stdin.nextInt();
+
+        for(int i = 1; i <= qtContract; ++i){
+            System.out.println("Enter contract #1 data: ");
+            System.out.println("Date (DD/MM;YYYY)");
+            /* DATE CALENDAR */
+            System.out.println("Value per hour: ");
+            double valuePerHour = stdin.nextDouble();
+            System.out.println("Duration");
+            int duration = stdin.nextInt();
+        }
+
+
+        WorkerLevel level1 = new Worker("s")
+
 
 
     }
--- ExercicioEnem_Composicao/src/main/java/WorkerLevel.java
+++ ExercicioEnem_Composicao/src/main/java/WorkerLevel.java
@@ -1,5 +1,22 @@
 public enum WorkerLevel {
-    JUNIOR,
-    MID_LEVEL,
-    SENIOR;
+
+     JUNIOR(1),
+     MID_LEVEL(2),
+     SENIOR(3);
+
+    private int code;
+
+    WorkerLevel (int code){
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
 }
+
+
